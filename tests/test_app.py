c45f23f034aaa2761198692534773b8109683fe7
gui-kratos007 <gui.souza2003oliveir@gmail.com>
2023-11-29 16:20:11 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,11 @@
 from fastapi.testclient import TestClient
-
 from fast_zero.app import app
 
-
 def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app)
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/')
+    response = client.get('/')  # Act
 
-    assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.status_code == 200  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+    
