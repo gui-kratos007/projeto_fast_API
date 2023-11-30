6361bbf5f4792d12bad582869b53adb307c0a6e0
gui-kratos007 <gui.souza2003oliveir@gmail.com>
2023-11-30 17:29:27 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -5,3 +5,21 @@ class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
+
+
+class Message(BaseModel):
+    detail: str
